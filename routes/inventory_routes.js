125fd3d57e5df9f19ccc946eeb023a5de9c6040e
Alishba Athar <136738882+AlishbaAthar17@users.noreply.github.com>
2025-05-02 19:07:38 +0500

    inventory items

--- routes/inventory_routes.js
+++ routes/inventory_routes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const {addInventory, editInventory, getAllInventory, getByID, deleteInventory} = require('../controllers/inventory')
+const {addInventory, editInventory, getAllInventory, getByID, deleteInventory, getInventoryItems, getInventoryItemsByInventoryId, editInventoryItem} = require('../controllers/inventory')
 const {validateUser, validateAdmin} = require('../middleware/auth_mv')
 
 router.post('/inventory/add', validateUser, validateAdmin, addInventory)
@@ -8,5 +8,9 @@ router.put('/inventory/:id', validateUser, validateAdmin, editInventory)
 router.get('/inventory', validateUser, validateAdmin, getAllInventory)
 router.get('/inventory/:id', validateUser, validateAdmin, getByID)
 router.delete('/inventory/:id', validateUser, validateAdmin, deleteInventory)
+
+router.get('/inventory-items', validateUser, validateAdmin, getInventoryItems);
+router.get('/inventory-items/:id', validateUser, validateAdmin, getInventoryItemsByInventoryId);
+router.put('/inventory-items/:id', validateUser, validateAdmin, editInventoryItem);
 
 module.exports = router
